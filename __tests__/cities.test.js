e50906455ac594980bc652cd6a7fb548359a184f
Emily Sellers <emilymelindasellers@gmail.com>
2022-11-21 21:19:17 -0800

    add GET /cities route

--- __tests__/cities.test.js
+++ __tests__/cities.test.js
@@ -21,4 +21,14 @@ describe('cities routes', () => {
       country: 'Canada',
     });
   });
+
+  it('/GET should return all cities', async () => {
+    const res = await request(app).get('/cities');
+    expect(res.status).toEqual(200);
+    expect(res.body[0]).toEqual({
+      id: '1',
+      cityName: 'Seattle',
+      country: 'USA',
+    });
+  });
 });
