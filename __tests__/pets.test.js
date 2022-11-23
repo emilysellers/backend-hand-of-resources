c1ff552c9bdc35ec532f5b303a7272b236fe027f
Emily Sellers <emilymelindasellers@gmail.com>
2022-11-22 21:38:42 -0800

    add GET /pets route

--- __tests__/pets.test.js
+++ __tests__/pets.test.js
@@ -16,8 +16,19 @@ describe('pets routes', () => {
     const resp = await request(app).post('/pets').send(newPet);
     expect(resp.status).toEqual(200);
     expect(resp.body).toEqual({
+      id: '6',
       name: 'Jets',
       type: 'tortoise',
+    });
+  });
+
+  it('GET /pets should return all pets', async () => {
+    const res = await request(app).get('/pets');
+    expect(res.status).toEqual(200);
+    expect(res.body[0]).toEqual({
+      id: '1',
+      name: 'Garbanzo',
+      type: 'terrier',
     });
   });
 });
